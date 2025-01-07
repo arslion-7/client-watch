89c2559d35ffdccc7c3805284fd5f4a0d02bc087
Arslan <arslion7@gmail.com>
2025-01-07 12:57:22 +0500

    Prevent error in ts initDeepAR

--- src/configs/deepARConfig.ts
+++ src/configs/deepARConfig.ts
@@ -3,6 +3,7 @@ import * as deepar from 'deepar';
 export const initDeepAR = async () => {
   const deepAR = await deepar.initialize({
     licenseKey: import.meta.env.VITE_DEEPAR_APP_KEY,
+    // @ts-expect-error
     previewElement: document.querySelector('#deepar-div'),
     effect: 'https://cdn.jsdelivr.net/npm/deepar/effects/aviators'
   });
